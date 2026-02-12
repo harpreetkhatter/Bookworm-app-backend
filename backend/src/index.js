1b2a22b80c1850ddbc3b1590fd5a66e16fe0e7f8
harpreetkhatter <khatterharpreet@gmail.com>
2026-02-12 16:02:55 +0530

    more book route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,13 @@ import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
 import { connectDB } from "./lib/db.js"
+
+import cors from "cors"
+
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 app.use("/api/auth",authRoutes)
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -7,29 +7,92 @@ import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/",protectRoute, async (req, res) => {
+router.post("/", protectRoute, async (req, res) => {
   try {
-    
-    const {title,caption,rating,image}=req.body;
+    const { title, caption, rating, image } = req.body;
 
-    if(!title || !caption || !rating || !image){
-      return res.status(400).json({message:"All fields are required"})
+    if (!title || !caption || !rating || !image) {
+      return res.status(400).json({ message: "All fields are required" });
     }
     //upload the image
     //save to db
-    const uploadResponse=await cloudinary.uploader.upload(image)
-    const imageUrl=uploadResponse.secure_url;
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
 
-    const newBook=new Book({
-      title,caption,rating,image:imageUrl,user:req.user._id
-    })
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      user: req.user._id,
+    });
     await newBook.save();
-    res.status(201).json(newBook)
-
+    res.status(201).json(newBook);
   } catch (error) {
-    console.log("Error in creating book", error)
-    res.status(500).json({message:"Internal server error"})
+    console.log("Error in creating book", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
+
+//pagination =>infinite loading
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    const books = (await Book.find())
+      .toSorted({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+    const totalBooks = await Book.countDocuments();
+    res.status(200).json({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+  } catch (error) {
+    console.log("Error in getting books", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+router.get("/user", protectRoute, async (req, res) => {
+  try {
+    const books = (await Book.find({ user: req.user._id })).toSorted({ createdAt: -1 });
+    res.status(200).json(books);
+  } catch (error) {
+    console.log("Error in getting user books", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    //check user is creator of book
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    //delete image from cloudinary
+    if (!book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (error) {
+        console.log("Error in deleting image from cloudinary", error);
+      }
+    }
+    await book.deleteOne();
+    res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleting book", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 
 export default router;
